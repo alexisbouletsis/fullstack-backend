2d136ca181b7dddc4d23f7e079c6df7cd71037be
u6845037871-max <u6845037871@gmail.com>
2026-04-30 12:52:13 +0300

    added !

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.post('/api/notes', (request, response) => {
 
   if (!body.content) {
     return response.status(400).json({
-      error: 'content missing',
+      error: 'content missing!',
     })
   }
 
