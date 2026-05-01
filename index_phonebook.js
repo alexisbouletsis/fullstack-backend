a2ff586a53d1f23116f1e8146da62c19bb8f53f9
alexisbouletsis <alexisbouletsis@gmail.com>
2026-05-01 20:03:12 +0300

    changes

--- index_phonebook.js
+++ index_phonebook.js
@@ -1,39 +1,50 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
+// const morgan = require('morgan');
 
-app.use(express.json());
+// app.use(express.json());
 // app.use(morgan('tiny'));
 
-morgan.token('body', (req) => { 
-  return JSON.stringify(req.body); 
-});
+// morgan.token('body', (req) => { 
+//   return JSON.stringify(req.body); 
+// });
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
   { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+      id: "1",
+      name: "Arto Hellas", 
+      number: "040-123456"
     },
     { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+      id: "2",
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
     },
     { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+      id: "3",
+      name: "Dan Abramov", 
+      number: "12-43-234345"
     },
     { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+      id: "4",
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
     }
 ]
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 app.use(express.static('dist'))
+app.use(express.json())
 
 
 app.get('/', (request, response) => {
@@ -105,10 +116,15 @@ app.post('/api/persons/', (req, res) => {
   persons = persons.concat(person);
   res.status(201).send(persons);
 
-  
 })
 
-const PORT = 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
